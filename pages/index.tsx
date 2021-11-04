6d99a4e915dbc3880905e43a2b8c556d5316d853
Aviel Naftali <dev.naftaliaviel@gmail.com>
2021-11-05 00:19:45 +0700

    basic csr

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 const Home: NextPage = () => (
-  <h1>Welcome Aviel</h1>
+  <>
+    <header>
+      <ul>
+        <li><Link href="/"><a>Home</a></Link></li>
+        <li><Link href="/blog"><a>Blog</a></Link></li>
+        <li><Link href="/users"><a>Users</a></Link></li>
+      </ul>
+    </header>
+    <h1>Welcome Aviel</h1>
+  </>
 );
 
 export default Home;
