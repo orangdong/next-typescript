b5fd5c6a94fa65ebb8f6e6ad90c2c8bb2ea7af2a
Aviel Naftali <dev.naftaliaviel@gmail.com>
2021-11-06 00:56:48 +0700

    jsx styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 const Home: NextPage = () => (
   <>
     <header>
-      <ul>
+      <ul style={{ listStyleType: 'none', display: 'flex' }}>
         <li><Link href="/"><a>Home</a></Link></li>
         <li><Link href="/blog"><a>Blog</a></Link></li>
         <li><Link href="/users"><a>Users</a></Link></li>
--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,5 +1,7 @@
 export default function Users() {
   return (
-    <div />
+    <>
+      <p>Ini halaman users</p>
+    </>
   );
 }
