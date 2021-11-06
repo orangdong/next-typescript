5ff6ab306cc9cce0e30b7c2c26028575fdd31f5e
Aviel Naftali <dev.naftaliaviel@gmail.com>
2021-11-06 18:57:51 +0700

    auto redirection

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,4 +1,15 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
 export default function Custom404() {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/');
+    }, 3000); // 3 seconds
+  }, []);
+
   return (
     <div>
       <p>Contoh custom 404</p>
