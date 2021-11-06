e657d6eb5ca0ea02708fc8922540645cbee0c353
Aviel Naftali <dev.naftaliaviel@gmail.com>
2021-11-06 18:25:57 +0700

    layouting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import type { NextPage } from 'next';
-import Header from '../components/Header';
+import Layout from '../components/Layout';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => (
-  <>
-    <Header />
+  <Layout>
     <h1 className={styles['home-title']}>Welcome Aviel</h1>
-  </>
+  </Layout>
+
 );
 
 export default Home;
