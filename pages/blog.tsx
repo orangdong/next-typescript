4fed7174dd9b8fdf49cb1fc2b9d581f84e2b67a4
Aviel Naftali <dev.naftaliaviel@gmail.com>
2021-11-12 18:36:56 +0700

    getServerSideProps

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,7 +1,45 @@
-export default function Blog() {
+import Layout from '../components/Layout';
+import styles from '../styles/Home.module.css';
+
+interface Post{
+  id: number;
+  title: string;
+  body: string;
+}
+
+interface BlogProps{
+  posts: Post[];
+}
+
+export default function Blog(props: BlogProps) {
+  const { posts } = props;
   return (
-    <div>
-      <p>ini blog</p>
-    </div>
+    <Layout title="Blog">
+      <div className={`${'mb-1'}`}>
+        <h1 className={styles['home-title']}>Blog</h1>
+        <p className={styles.subtitle}>
+          Find news, updates, and best practices for cloud solutions and technologies.
+        </p>
+      </div>
+      <div className={`${'mb-1'}`}>
+        {posts.map((post) => (
+          <div className={styles.blog}>
+            <h4 style={{ marginTop: `${-3}px` }}>{post.title}</h4>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
+    </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const posts = await res.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
 }
